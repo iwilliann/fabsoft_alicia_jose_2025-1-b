7c1f872297bd7caadeec1ad2e3563a684d2130ff
iwilliann <willianalves@gmail.com>
2025-05-19 21:12:43 -0300

    API update

--- projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/controller/EmprestimoController.java
+++ projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/controller/EmprestimoController.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.univille.projfabsoftescolamusica.entity.Emprestimo;
 import br.univille.projfabsoftescolamusica.service.EmprestimoService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@RestController
+@RequestMapping("/api/v1/emprestimo")
 public class EmprestimoController {
     @Autowired
     private EmprestimoService service;
